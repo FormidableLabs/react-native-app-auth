1ffebb7ce91bfbcc33deb697bc0dbc233c02fd4f
Jan-Pieter Zoutewelle <jp@intreba.nl>
2018-02-20 13:10:44 +0100

    Fix typos and add prettier

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,44 @@
+export interface BaseAuthConfiguration {
+  issuer: string;
+  clientId: string;
+}
 export interface AuthConfiguration extends BaseAuthConfiguration {
-    scopes: string[];
-    redirectUrl: string;
-    aditionalParameters?: {[name: string]: string};
-  }
+  scopes: string[];
+  redirectUrl: string;
+  additionalParameters?: { [name: string]: string };
+}
 
-  export interface BaseAuthConfiguration{
-    issuer: string;
-    clientId: string;
-  }
+export interface RevokeConfiguration {
+  clientId: string;
+  issuer: string;
+}
 
-  export interface RevokeConfiguration{
-    clientId: string;
-    issuer: string;
-  }
+export interface AuthorizeResult {
+  accessToken: string;
+  accessTokenExpirationDate: string;
+  additionalParameters?: { [name: string]: string };
+  idToken: string;
+  refreshToken: string;
+  tokenType: string;
+}
 
-  export interface AuthorizeResult {
-    accessToken: string;
-    accessTokenExpirationDate: string;
-    aditionalParameters?: {[name: string]: string};
-    idToken: string;
-    refreshToken: string;
-    tokenType: string;
-  }
+export interface RevokeConfiguration {
+  tokenToRevoke: string;
+  sendClientId?: boolean;
+}
 
-  export interface RevokeConfiguration {
-    tokenToRevoke: string;
-    sendClientId?: boolean;
-  }
+export interface RefreshConfiguration {
+  refreshToken: string;
+}
 
-  export interface RefreshConfiguration {
-    refreshToken: string;
-  }
+export function authorize(config: AuthConfiguration): Promise<AuthorizeResult>;
 
-  export function authorize(config: AuthConfiguration): Promise<AuthorizeResult>;
+export function refresh(
+  config: AuthConfiguration,
+  refreshConfig: RefreshConfiguration
+): Promise<AuthorizeResult>;
 
-  export function refresh(config: AuthConfiguration, refreshConfig: RefreshConfiguration): Promise<AuthorizeResult>;
-
-  export function revoke(config: BaseAuthConfiguration, revokeConfig: RevokeConfiguration): Promise<void>;
+export function revoke(
+  config: BaseAuthConfiguration,
+  revokeConfig: RevokeConfiguration
+): Promise<void>;
