6ebefa4b4bccc9585ed7ffd6252d5697b595ca7c
Kadi Kraman <kadi.kraman@formidable.com>
2018-10-11 11:59:19 +0100

    Add an android example

---
+++ Example/AndroidExample/components/ButtonContainer.js
@@ -0,0 +1,13 @@
+// @flow
+
+import styled from 'styled-components/native';
+
+export default styled.View`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  align-self: flex-end;
+  flex-direction: row;
+  margin: 5px;
+`;
