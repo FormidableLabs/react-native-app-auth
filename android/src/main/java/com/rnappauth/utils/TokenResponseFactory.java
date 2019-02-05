09e02ae5360e1675e8b9f91d3181e0af405209f7
Kadi Kraman <kadi.kraman@formidable.com>
2019-02-05 11:52:50 -0800

    Refactor additional parameters and scope creation into helper methods

--- android/src/main/java/com/rnappauth/utils/TokenResponseFactory.java
+++ android/src/main/java/com/rnappauth/utils/TokenResponseFactory.java
@@ -8,8 +8,40 @@ import net.openid.appauth.AuthorizationResponse;
 import net.openid.appauth.TokenResponse;
 
 import java.util.Iterator;
+import java.util.Map;
 
 public final class TokenResponseFactory {
+    private static final WritableMap createAdditionalParametersMap(Map<String, String> additionalParameters) {
+        WritableMap additionalParametersMap = Arguments.createMap();
+
+        if (!additionalParameters.isEmpty()) {
+
+            Iterator<String> iterator = additionalParameters.keySet().iterator();
+
+            while(iterator.hasNext()) {
+                String key = iterator.next();
+                additionalParametersMap.putString(key, additionalParameters.get(key));
+            }
+        }
+
+        return additionalParametersMap;
+    }
+
+    private static final WritableArray createScopeArray(String scope) {
+        WritableArray scopeArray = Arguments.createArray();
+        if (!scope.isEmpty()) {
+            String[] scopesArray = scope.split(" ");
+
+            for( int i = 0; i < scopesArray.length - 1; i++)
+            {
+                scopeArray.pushString(scopesArray[i]);
+            }
+        }
+
+        return scopeArray;
+    }
+
+
     /*
      * Read raw token response into a React Native map to be passed down the bridge
      */
@@ -17,27 +49,14 @@ public final class TokenResponseFactory {
         WritableMap map = Arguments.createMap();
 
         map.putString("accessToken", response.accessToken);
+        map.putMap("additionalParameters", createAdditionalParametersMap(response.additionalParameters));
+        map.putString("idToken", response.idToken);
+        map.putString("refreshToken", response.refreshToken);
+        map.putString("tokenType", response.tokenType);
 
         if (response.accessTokenExpirationTime != null) {
             map.putString("accessTokenExpirationDate", DateUtil.formatTimestamp(response.accessTokenExpirationTime));
         }
-
-        WritableMap additionalParametersMap = Arguments.createMap();
-
-        if (!response.additionalParameters.isEmpty()) {
-
-            Iterator<String> iterator = response.additionalParameters.keySet().iterator();
-
-            while(iterator.hasNext()) {
-                String key = iterator.next();
-                additionalParametersMap.putString(key, response.additionalParameters.get(key));
-            }
-        }
-
-        map.putMap("additionalParameters", additionalParametersMap);
-        map.putString("idToken", response.idToken);
-        map.putString("refreshToken", response.refreshToken);
-        map.putString("tokenType", response.tokenType);
 
         return map;
     }
@@ -49,52 +68,17 @@ public final class TokenResponseFactory {
         WritableMap map = Arguments.createMap();
 
         map.putString("accessToken", response.accessToken);
+        map.putMap("authorizeAdditionalParameters", createAdditionalParametersMap(authResponse.additionalParameters));
+        map.putMap("tokenAdditionalParameters", createAdditionalParametersMap(response.additionalParameters));
+        map.putString("idToken", response.idToken);
+        map.putString("refreshToken", response.refreshToken);
+        map.putString("tokenType", response.tokenType);
+        map.putArray("scopes", createScopeArray(authResponse.scope));
 
         if (response.accessTokenExpirationTime != null) {
             map.putString("accessTokenExpirationDate", DateUtil.formatTimestamp(response.accessTokenExpirationTime));
         }
 
-        WritableMap authorizeAdditionalParameters = Arguments.createMap();
-
-        if (!authResponse.additionalParameters.isEmpty()) {
-
-            Iterator<String> iterator = authResponse.additionalParameters.keySet().iterator();
-
-            while(iterator.hasNext()) {
-                String key = iterator.next();
-                authorizeAdditionalParameters.putString(key, authResponse.additionalParameters.get(key));
-            }
-        }
-
-        WritableMap tokenAdditionalParameters = Arguments.createMap();
-
-        if (!response.additionalParameters.isEmpty()) {
-
-            Iterator<String> iterator = response.additionalParameters.keySet().iterator();
-
-            while(iterator.hasNext()) {
-                String key = iterator.next();
-                tokenAdditionalParameters.putString(key, response.additionalParameters.get(key));
-            }
-        }
-
-        map.putMap("authorizeAdditionalParameters", authorizeAdditionalParameters);
-        map.putMap("tokenAdditionalParameters", tokenAdditionalParameters);
-        map.putString("idToken", response.idToken);
-        map.putString("refreshToken", response.refreshToken);
-        map.putString("tokenType", response.tokenType);
-
-        if (!authResponse.scope.isEmpty()) {
-            WritableArray scopes = Arguments.createArray();
-            String[] scopesArray = authResponse.scope.split(" ");
-
-            for( int i = 0; i < scopesArray.length - 1; i++)
-            {
-                scopes.pushString(scopesArray[i]);
-            }
-
-            map.putArray("scopes", scopes);
-        }
 
         return map;
     }
