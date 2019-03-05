05551de4181eefaef7b819ee99837ecaa757e42f
Nihau <nhauseux@prismamedia.com>
2019-03-05 22:46:57 +0100

    custom connection builder. Allow to pass custom headers to oauth requests.

---
+++ android/src/main/java/com/rnappauth/utils/CustomConnectionBuilder.java
@@ -0,0 +1,58 @@
+package com.rnappauth.utils;
+
+/*
+ * Copyright 2016 The AppAuth for Android Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import net.openid.appauth.connectivity.ConnectionBuilder;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+
+/**
+ * An implementation of {@link ConnectionBuilder} that permits
+ * to set custom headers on connection use to request endpoints.
+ * Useful for non-spec compliant oauth providers.
+ */
+public final class CustomConnectionBuilder implements ConnectionBuilder {
+
+    private Map<String, String> headers = null;
+    private ConnectionBuilder connectionBuilder;
+
+    public CustomConnectionBuilder(ConnectionBuilder connectionBuilderToUse) {
+        connectionBuilder = connectionBuilderToUse;
+    }
+
+    public void setHeaders (Map<String, String> headersToSet) {
+        headers = headersToSet;
+    }
+
+    @NonNull
+    @Override
+    public HttpURLConnection openConnection(@NonNull Uri uri) throws IOException {
+        HttpURLConnection conn = connectionBuilder.openConnection(uri);
+        if (headers != null) {
+            for (Map.Entry<String, String> header: headers.entrySet()) {
+                conn.setRequestProperty(header.getKey(), header.getValue());
+            }
+        }
+
+        return conn;
+    }
+}
