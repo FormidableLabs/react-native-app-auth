be1a80a0b70aa52282d5e98cb103ad3808085f9a
stbhb <104437822+stbhb@users.noreply.github.com>
2022-06-08 13:13:53 +0200

    Pass additional param ui_locales

--- android/src/main/java/com/rnappauth/RNAppAuthModule.java
+++ android/src/main/java/com/rnappauth/RNAppAuthModule.java
@@ -674,6 +674,10 @@ public class RNAppAuthModule extends ReactContextBaseJavaModule implements Activ
                 authRequestBuilder.setState(additionalParametersMap.get("state"));
                 additionalParametersMap.remove("state");
             }
+            if (additionalParametersMap.containsKey("ui_locales")) {
+                authRequestBuilder.setUiLocales(additionalParametersMap.get("ui_locales"));
+                additionalParametersMap.remove("ui_locales");
+            }
 
             authRequestBuilder.setAdditionalParameters(additionalParametersMap);
         }
