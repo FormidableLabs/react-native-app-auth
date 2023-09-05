317631a3e31bde8e1587622a6ef308c78e69a2ec
Rob Walker <hi@robwalker.co>
2023-09-05 16:23:36 +0100

    chore: Update Example app to RN 0.72

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,28 +1,20 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-// As the example project uses `link:../` for react-native-owl, which creates a symlink, we need to manually map the project so it is properly used my Metro.
-// This will not be required by other projects using react-native-owl installed from a package repository.
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const path = require('path');
 
 const extraNodeModules = {
   'react-native-app-auth': path.resolve(path.join(__dirname, '..')),
 };
 const watchFolders = [path.resolve(path.join(__dirname, '..'))];
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
+const config = {
   resolver: {
     extraNodeModules: new Proxy(extraNodeModules, {
       get: (target, name) =>
@@ -33,3 +25,5 @@ module.exports = {
   },
   watchFolders,
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
