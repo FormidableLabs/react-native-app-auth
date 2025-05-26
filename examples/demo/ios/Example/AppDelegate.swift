682c7760c26e49fe3bad33aa521640d196d45fcb
Eli Zibin <1131641+zibs@users.noreply.github.com>
2025-05-26 13:02:44 -0700

    upgrade demo app
    - replaces existing demo app with newly bootstrapped app to avoid painstaking upgrade.
    - replaces old UI with simpler more basic UI.
    - uses Swift on iOS now
    - adds bridging header for obj-c interop

---
+++ examples/demo/ios/Example/AppDelegate.swift
@@ -0,0 +1,73 @@
+import React
+import ReactAppDependencyProvider
+import React_RCTAppDelegate
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate,
+  RNAppAuthAuthorizationFlowManager
+{
+  var window: UIWindow?
+
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+  // Required by RNAppAuthAuthorizationFlowManager protocol
+  public weak var authorizationFlowManagerDelegate:
+    RNAppAuthAuthorizationFlowManagerDelegate?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication
+      .LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "Example",
+      in: window,
+      launchOptions: launchOptions
+    )
+
+    return true
+  }
+
+  // Handle OAuth redirect URL
+  func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    if let authorizationFlowManagerDelegate = self
+      .authorizationFlowManagerDelegate
+    {
+      if authorizationFlowManagerDelegate.resumeExternalUserAgentFlow(with: url)
+      {
+        return true
+      }
+    }
+    return false
+  }
+
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+    #if DEBUG
+      RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
+  }
+}
---
+++ examples/demo/ios/RNAppAuthBridge.h
@@ -0,0 +1,8 @@
+//
+//  RNAppAuthBridge.h
+//  Example
+//
+//  Created by Eli Zibin on 2025-05-26.
+//
+
+#import "RNAppAuthAuthorizationFlowManager.h"
